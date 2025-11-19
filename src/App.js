f7aad746cfc4532e72b4928b6b66f72184fb57c0
Murali <mmurali4756@gmail.com>
2025-11-19 19:33:38 +0530

    remove unwanted data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
 
-/**
- * 1. Add new item to TODO list on click of "+" button from input box.
- * 2. Complete an item on checkbox selection.
- * 3. Remove the items on click of delete button.
- * 4. You can't delete the completed items.
- * 5. Apply necessary valdations wherever needed.
- * 6. Try to use smart syntax, wherever possible.
- * 7. Do not bother about CSS.
- * 8. Pick whichever point you wish to implement first.
- * 9. **optional**: Move Add TODO feature in a seperate component.
- */
-
-const Listitems = [
-  { text: "Learn JavaScriptss", done: false },
-  { text: "Learn React", done: false },
-  { text: "Play around in codesandbox", done: true },
-  { text: "Build something awesome", done: true },
-];
-
 export default function App() {
-  const [items, setItems] = useState(Listitems);
+  const [items, setItems] = useState([
+    { text: "Learn JavaScriptss", done: false },
+    { text: "Learn React", done: false },
+    { text: "Play around in codesandbox", done: true },
+    { text: "Build something awesome", done: true },
+  ]);
   const [todo, setTodo] = useState("");
   const addTodo = () => {
-    // console.log(e.target.value);
     if (todo) {
       const todoItem = {
         text: todo,
@@ -53,8 +38,6 @@ export default function App() {
     }
     setItems((prev) => prev.filter((item) => item.text !== param.text));
   };
-  console.log("item", items);
-  console.log("todo", todo);
   return (
     <div className="App">
       <p className="title">Todo</p>
